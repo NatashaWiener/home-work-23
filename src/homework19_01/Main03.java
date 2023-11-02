062923f9579ef23a90cc6a5ade3003b79424140f
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-02 10:36:39 +0100

    change Main03.java

--- src/homework19_01/Main03.java
+++ src/homework19_01/Main03.java
@@ -1,3 +1,4 @@
+
 package homework19_01;
 
 import java.util.Scanner;
@@ -13,7 +14,8 @@ public class Main03 {
           System.out.println("Enter natural number");
           number = scanner.nextInt();
         } while (number <= 0);
-        System.out.println("Your natural number: " + number);
+             System.out.println("vnesla izmeneniya v 10:12");       
+            System.out.println("Your natural number: " + number);
       }
     }
 
