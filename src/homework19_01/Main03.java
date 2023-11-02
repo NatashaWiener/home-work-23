b8101d3fb5ebae4df168248ca81710e6827c6509
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-02 10:04:04 +0100

    add some code

---
+++ src/homework19_01/Main03.java
@@ -0,0 +1,22 @@
+package homework19_01;
+
+import java.util.Scanner;
+
+public class Main03 {
+
+
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        do {
+          System.out.println("Enter natural number");
+          number = scanner.nextInt();
+        } while (number <= 0);
+        System.out.println("Your natural number: " + number);
+      }
+    }
+
+
+
+
